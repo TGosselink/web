ad1382dc2d66def2d3696f24f4e85277ddb5190d
Theo Gosselink <t.gosselink@breman.nl>
2016-12-23 12:24:06 +0100

    Tik foutje.

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
     echo "<hr>";
     $a = "Dit is een test van Theo Gosselink...<br>";
     echo $a;
-    echo "De WEB-master branche<br>
+    echo "De WEB-master branche<br>";
     echo "Deel 2<br>";
 ?>
